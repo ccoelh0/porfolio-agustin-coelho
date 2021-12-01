eef4d866b684a27707ccd7623f4a5819e74bf165
unknown <coelhoagustin1@gmail.com>
2021-12-01 00:37:41 -0300

    cambios en portada

--- src/components/portada/Portada.js
+++ src/components/portada/Portada.js
@@ -7,24 +7,28 @@ export default function Portada() {
       <div className="grid">
         <div className="main">
           <div className="main-content">
-            <p>{"<h1>"}</p>
-            <h2>
-              Hola,
-              <br />
-              bienvenido a mi
-              <br />
-              <span>
-                <Typewriter
-                  options={{
-                    strings: ["porfolio"],
-                    autoStart: true,
-                    pauseFor: 5000,
-                    loop: true,
-                  }}
-                />
-              </span>
-            </h2>
-            <p>{"</h1>"}</p>
+            <p className="main-content-p">{"<main>"}</p>
+            <div style={{ marginLeft: "30px" }}>
+              <p className="main-content-p">{"<h1>"}</p>
+              <h2>
+                Hola,
+                <br />
+                bienvenido a mi
+                <br />
+                <span>
+                  <Typewriter
+                    options={{
+                      strings: ["porfolio"],
+                      autoStart: true,
+                      pauseFor: 5000,
+                      loop: true,
+                    }}
+                  />
+                </span>
+              </h2>
+              <p className="main-content-p">{"</h1>"}</p>
+            </div>
+            <p className="main-content-p">{"</main>"}</p>
           </div>
         </div>
         <div className="main-img"></div>
